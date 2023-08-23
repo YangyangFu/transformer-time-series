bb7087d27f622b98d315bf42bebe3496b66d8378
Yangyang Fu <fuyy2008@gmail.com>
2023-08-23 12:19:36 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='tsl',
+    version='1.0',
+    packages=setuptools.find_packages(),
+    url='github.com/yangyangfu/transformer-time-series',
+    license='GPL-3.0',
+    author='Yangyang Fu',
+    description='Timeseries forcasting library using transformer models implemented in Tensorflow'
+)
