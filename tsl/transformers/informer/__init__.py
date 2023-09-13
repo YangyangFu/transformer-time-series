5f6486d148918d6f9a250a80722c1366321bd6a6
Yangyang Fu <fuyy2008@gmail.com>
2023-09-13 09:47:49 -0500

    update package

--- tsl/transformers/informer/__init__.py
+++ tsl/transformers/informer/__init__.py
@@ -1,5 +1,3 @@
-from .time_features import TimeCovariates
-from .data_loader import DataLoader
 from .multihead_attention import MultiHeadAttention
 from .multihead_probsparse_attention import MultiHeadProbSparseAttention
 from .preprocessor import PositionalEmbedding, TemporalEmbedding, CategoricalEmbedding, TimeFeatureEmbedding
